def135a3eff7c7e5e17cafcb8be4fb8b5e85da7a
Alex <mexi1415@gmail.com>
2024-05-30 09:25:02 -0500

    add a sleep before read container logs

--- local_docker/vars.tf
+++ local_docker/vars.tf
@@ -1,5 +1,10 @@
+resource "time_sleep" "sleeping" {
+  create_duration = "5s"
+  depends_on = [ docker_container.web ]
+}
 
 data "docker_logs" "container_log" {
+  depends_on = [ time_sleep.sleeping ]
   for_each = { for c in docker_container.web : c.name => c }
   name = each.value.name
 }
